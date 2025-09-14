cef4c0e1a0ace6129edd037be142375434834a78
ferreyraal <ferreyraal@gmail.com>
2025-09-13 23:52:57 -0300

    temple images update 4

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,24 +1,9 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('nav ul');
 
-    const menuApp = document.createElement('button');
-    menuApp.innerHTML = '&#9776;'; 
-    menuApp.classList.add('menuApp');
-    menuApp.setAttribute('aria-label', 'Toggle Navigation');
+const webButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
 
-
-    const header = document.querySelector('header');
-    header.insertBefore(menuApp, nav);
-
-
-    menuApp.addEventListener('click', () => {
-        nav.classList.toggle('show');
-
-        if (nav.classList.contains('show')) {
-            menuApp.innerHTML = '&times;'; 
-        } else {
-            menuApp.innerHTML = '&#9776;';
-        }
-    });
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    webButton.classList.toggle('open');
 });
