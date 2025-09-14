f5d76118cd8add27ab84aef0068e1b9ee457ac8b
ferreyraal <ferreyraal@gmail.com>
2025-09-14 00:01:33 -0300

    temple image update 8

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,17 @@ webButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     webButton.classList.toggle('open');
 });
+
+
+
+
+
+
+const currentYear = new Date().getFullYear();
+
+const yearSpan = document.getElementById('year');
+
+yearSpan.textContent = currentYear;
+
+const lastModifiedElement = document.getElementById("lastModified");
+lastModifiedElement.textContent = document.lastModified;
