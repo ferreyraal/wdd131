f9ca3be7e61628892e2837d25ace5a1c4317b241
ferreyraal <ferreyraal@gmail.com>
2025-09-13 23:56:15 -0300

    temple image update 6

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,7 +3,7 @@
 const webButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
+webButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     webButton.classList.toggle('open');
 });
