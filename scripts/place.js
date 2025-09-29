e83dcad372d984bfcce79b30eb95c5c9b6918d47
ferreyraal <ferreyraal@gmail.com>
2025-09-29 12:27:40 -0300

    lazy

--- scripts/place.js
+++ scripts/place.js
@@ -1,20 +1,20 @@
 const temperature = 10;
 const windSpeed = 5; 
 
-function calculateWindChill(temp, wind) {
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(wind, 0.16) +
-        0.3965 * temp * Math.pow(wind, 0.16)
-    ).toFixed(1);
-}
+    function calculateWindChill(temp, wind) {
+        return (
+            13.12 +
+            0.6215 * temp -
+            11.37 * Math.pow(wind, 0.16) +
+            0.3965 * temp * Math.pow(wind, 0.16)
+        ).toFixed(1);
+    }
 
-let windChillText = "N/A";
-if (temperature <= 10 && windSpeed > 4.8) {
-    windChillText = calculateWindChill(temperature, windSpeed) + " °C";
-}
+    let windChillText = "N/A";
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windChillText = calculateWindChill(temperature, windSpeed) + " °C";
+    }
 
-document.getElementById("windchill").textContent = windChillText;
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+    document.getElementById("windchill").textContent = windChillText;
+    document.getElementById("year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = document.lastModified;
